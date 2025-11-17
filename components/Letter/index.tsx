03b9990156ca99be2f4ed80aa591bc464c2e8b69
806gw <shjea4364@naver.com>
2025-11-17 09:14:52 +0900

    chore: 로그아웃 우선 제거

--- components/Letter/index.tsx
+++ components/Letter/index.tsx
@@ -4,8 +4,8 @@ import { useState } from "react";
 import Step1 from "./Step1";
 import Step2 from "./Step2";
 import { Toastify } from "../Toastify";
-import { logout } from "@/services/auth/auth.api";
-import * as styles from "./style.css"
+// import { logout } from "@/services/auth/auth.api";                          
+// import * as styles from "./style.css"
 
 interface Teacher {
   id: string;
@@ -35,15 +35,15 @@ export default function Letter() {
     }
   };
 
-  const handleLogout = () => {
-    logout()
-  }
+  // const handleLogout = () => {
+  //   logout()
+  // }
 
   return (
     <div>
-      <div className={styles.logoutContainer}>
+      {/* <div className={styles.logoutContainer}>
         <button className={styles.logoutButton} onClick={handleLogout}>로그아웃</button>
-      </div>
+      </div> */}
       {step === 1 && <Step1 onNext={handleStep1Complete} />}
       {step === 2 && selectedTeacher && (
         <Step2
